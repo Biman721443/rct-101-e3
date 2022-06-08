a26a26e6acd2361acab6fe0bfc18f9cfc9d1efe7
Biman <bimangharai721443@gmail.com>
2022-06-08 09:12:14 +0530

    submit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import React from "react";
 import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      {
-        // Code here
-      }
+     <Home/>
+      
     </div>
+
+  
   );
 }
 
